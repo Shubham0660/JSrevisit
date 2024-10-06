2d67231b44bbc36e66b7b6ebb2fff4ef3e069b28
Shubham <149568689+Shubham0660@users.noreply.github.com>
2024-10-06 04:36:36 +0000

    config done

---
+++ 01_Basics/DataTypes.js
@@ -0,0 +1,9 @@
+let num=33
+
+let num1="33abc";
+let num3=null;
+
+let numberValue=Number(num1);
+
+console.log(numberValue)
+console.log(typeof(numberValue));
---
+++ 01_Basics/TypeConversion.js
@@ -0,0 +1,14 @@
+let isLoggedIn=1;
+
+let booleanisLoggedIn=Boolean(isLoggedIn);
+
+console.log(booleanisLoggedIn);
+
+console.log(typeof(booleanisLoggedIn));
+
+let age=22;
+
+let Stringage=String(age);
+
+console.log(Stringage)
+console.log(typeof(Stringage));
